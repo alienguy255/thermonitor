4d7890be5d0b68d3a84ebd194a3a7ce1e6e38b14
slaplante <scottmlaplante@gmail.com>
2019-10-27 11:13:29 -0400

    Adjust connect and read timeouts

--- src/main/java/org/scottsoft/monitor/config/AsyncExceptionHandler.java
+++ src/main/java/org/scottsoft/monitor/config/AsyncExceptionHandler.java
@@ -3,8 +3,10 @@ package org.scottsoft.monitor.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.lang.reflect.Method;
+import java.net.SocketTimeoutException;
 
 @Slf4j
 @Component
@@ -12,7 +14,11 @@ public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
 
     @Override
     public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-        log.warn("An error occurred running async task.", ex);
+        if (ex instanceof ResourceAccessException && ex.getCause() instanceof SocketTimeoutException) {
+            log.warn("Timeout occurred running async task: {}", ex.getMessage());
+        } else {
+            log.warn("An error occurred running async task.", ex);
+        }
     }
 
 }
